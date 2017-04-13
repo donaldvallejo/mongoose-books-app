f7af2d21667571d6ca65bf37dcfd71a6255d4470
donny <donny.vallejo@gmail.com>
2017-04-12 18:22:50 -0700

    adding my CRUD's

--- models/index.js
+++ models/index.js
@@ -1,2 +1,8 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/book-app");
+
+module.exports.Book = require("./books.js");
+// module.exports.Author = require("./author.js");
+// module.exports.Gnome = require("./gnome.js");
+var Schema = mongoose.Schema;
+module.exports.Book = require("./books.js");
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // server.js
 // SERVER-SIDE JAVASCRIPT
 
-
 /////////////////////////////
 //  SETUP and CONFIGURATION
 /////////////////////////////
@@ -9,6 +8,7 @@
 //require express in our app
 var express = require('express'),
   bodyParser = require('body-parser');
+  var db = require('./models')
 
 // generate a new express app and call it 'app'
 var app = express();
@@ -19,51 +19,53 @@ app.use(express.static('public'));
 // body parser config to accept our datatypes
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
 ////////////////////
 //  DATA
 ///////////////////
 
-var books = [
-  {
-    _id: 15,
-    title: "The Four Hour Workweek",
-    author: "Tim Ferriss",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/four_hour_work_week.jpg",
-    release_date: "April 1, 2007"
-  },
-  {
-    _id: 16,
-    title: "Of Mice and Men",
-    author: "John Steinbeck",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/of_mice_and_men.jpg",
-    release_date: "Unknown 1937"
-  },
-  {
-    _id: 17,
-    title: "Romeo and Juliet",
-    author: "William Shakespeare",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/romeo_and_juliet.jpg",
-    release_date: "Unknown 1597"
-  }
-];
-
-
-var newBookUUID = 18;
-
-
-
-
-
-
-
-////////////////////
-//  ROUTES
-///////////////////
-
-
-
+// server.js
+app.get('/api/books', function (req, res) {
+  // send all books as JSON response
+  db.Book.find(function(err, books){
+    if (err) {
+      console.log("index error: " + err);
+      res.sendStatus(500);
+    }
+    res.json(books);
+  });
+});
+db.Book.find({}, function(err, Books) {
+        res.json({ books_list });
+// var books = [
+//   {
+//     _id: 15,
+//     title: "The Four Hour Workweek",
+//     author: "Tim Ferriss",
+//     image: "https://s3-us-west-2.amazonaws.com/sandboxapi/four_hour_work_week.jpg",
+//     release_date: "April 1, 2007"
+//   },
+//   {
+//     _id: 16,
+//     title: "Of Mice and Men",
+//     author: "John Steinbeck",
+//     image: "https://s3-us-west-2.amazonaws.com/sandboxapi/of_mice_and_men.jpg",
+//     release_date: "Unknown 1937"
+//   },
+//   {
+//     _id: 17,
+//     title: "Romeo and Juliet",
+//     author: "William Shakespeare",
+//     image: "https://s3-us-west-2.amazonaws.com/sandboxapi/romeo_and_juliet.jpg",
+//     release_date: "Unknown 1597"
+//   }
+// ];
+//
+//
+// var newBookUUID = 18;
+//
+// ////////////////////
+// //  ROUTES
+// ///////////////////
 
 // define a root route: localhost:3000/
 app.get('/', function (req, res) {
@@ -128,10 +130,6 @@ app.delete('/api/books/:id', function (req, res) {
   books.splice(deleteBookIndex, 1);
   res.json(bookToDelete);
 });
-
-
-
-
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Book app listening at http://localhost:3000/');
